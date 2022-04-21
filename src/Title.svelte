35a9de37d980bb68ba83b01c7c307dc19bd5bbf3
Alastair Otter <alastair@mediahack.co.za>
2022-04-21 16:25:23 +0200

    Adding social media links

--- src/Title.svelte
+++ src/Title.svelte
@@ -1,4 +1,13 @@
 <div class="header">
+  <div class="social">
+    <a
+      href="https://twitter.com/intent/tweet?text=A%20visualisation%20of%20two%20years%20of%20Covid%20in%20South%20Africa.%20https://covid.theoutlier.co.za"
+      target="_blank"><img src="/twitter.svg" alt="Share to Twitter" /></a
+    >&nbsp;<a
+      href="https://www.facebook.com/sharer/sharer.php?u=https://covid.theoutlier.co.za"
+      target="_blank"><img src="/facebook.svg" alt="Share to Facebook" /></a
+    >
+  </div>
   <div class="logo">
     <a href="https://theoutlier.co.za" target="_blank"
       ><img src="/outlier-logo.png" alt="Outlier Logo" /></a
@@ -59,5 +68,19 @@
     max-width: 500px;
     margin-left: auto;
     margin-right: auto;
+  }
+  .social {
+    margin-top: 20px;
+    font-size: 0.8rem;
+    width: 100%;
+    text-align: center;
+    text-transform: uppercase;
+    margin-bottom: 50px;
+    font-family: 'Inter', Arial, Helvetica, sans-serif;
+    color: #c4c4a3;
+  }
+  .social img {
+    transform: translate(0px, 3px);
+    width: 30px;
   }
 </style>
